c4759d1d0f248582d2397a400314c78bf2b0066a
Husienrifa <136290439+Husienrifa@users.noreply.github.com>
2026-03-11 16:48:38 +0800

    Create config.py for API configuration and environment variables

---
+++ config.py
@@ -0,0 +1,7 @@
+# API Configuration and Environment Variables
+
+# Current Date and Time (UTC)
+CURRENT_DATE_TIME = "2026-03-11 08:48:17"
+
+# Current User's Login
+CURRENT_USER_LOGIN = "Husienrifa"
